c6b5a0f9fd323913a17d79eb1d90462f6e199010
Kevin Ernest Long <kevinelong@gmail.com>
2025-05-08 11:31:13 -0700

    update

--- src/Deck.java
+++ src/Deck.java
@@ -7,27 +7,43 @@ public class Deck { //acting like a Game class
     Deck() {
         this.cards = new ArrayList<>();
         String[] suits = {"Hearts", "Spades", "Diamonds", "Clubs"};
-        String[] values = {"2","3","4","5","6","7","8","9","10","J","Q","K","A"};
+        String[] values = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
         //create all cards
-        for(String s : suits){ //four kinds
-            for(String v : values){
-                this.cards.add(new Card(s,v));
+        for (String s : suits) { //four kinds
+            for (int i = 0; i < values.length; i++) {
+
+//                int n = i + 2;//current index + 2 because "2" is the zeroth
+//                if ("JQK".contains(values[i])) {
+//                    n = 10;
+//                } else if ("A".contains(values[i])) {
+//                    n = 11;
+//                }
+
+                int n =  switch (values[i]){
+                    case "K", "J", "Q" -> 10;
+                    case "A" -> 11;
+                    default -> i + 2; //current index + 2 because "2" is the zeroth
+                };
+                this.cards.add(new Card(s, values[i], n));
             }
         }
     }
-    public void shuffle(){
+
+    public void shuffle() {
         Collections.shuffle(this.cards);
     }
-    public Card dealCard(){
+
+    public Card dealCard() {
         //deal off the top
 //        return cards.isEmpty() ? null : cards.remove(0);
-        if(cards.size() > 0){
+        if (cards.size() > 0) {
             return cards.remove(0);
         } else {
             return null;
         }
     }
-    public int getSize(){
+
+    public int getSize() {
         return this.cards.size();
     }
 }
--- src/MainApp.java
+++ src/MainApp.java
@@ -2,16 +2,32 @@ public class MainApp {
     public static void main(String[] args) {
         Deck deck = new Deck();
         Hand hand1 = new Hand();
+        Hand hand2 = new Hand();
 
         //random - stochastic stats - gaussian visual - brownian moving
         deck.shuffle();
 
-        int CARDS_PER_HAND = 5;
+        int CARDS_PER_HAND = 2;
         for (int i = 0; i < CARDS_PER_HAND; i++) {
             hand1.pickUp(deck.dealCard());
         }
 
+        for (int i = 0; i < CARDS_PER_HAND; i++) {
+            hand2.pickUp(deck.dealCard());
+        }
+
         int handValue = hand1.getValue();
         System.out.println("Worth: " + handValue);
+
+        int handValue2 = hand2.getValue();
+        System.out.println("Worth2: " + handValue2);
+
+        if(handValue > handValue2){
+            System.out.println("Player 1 wins!!!");
+        }else if(handValue < handValue2){
+            System.out.println("Player 2 wins!!!");
+        }else{
+            System.out.println("Tie!!!");
+        }
     }
 }
